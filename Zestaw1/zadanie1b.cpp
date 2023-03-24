ebae676ab2d871f2cd97b94066b84ac72c85b2b2
Patryk Lach <patryk.lach@mp.pl>
2023-03-24 09:12:33 +0100

    fix(zadanie1): odd and even border fix

--- Zestaw1/zadanie1b.cpp
+++ Zestaw1/zadanie1b.cpp
@@ -15,8 +15,8 @@ int main()
     cout << "Podaj rozmiar wewnetrzny: ";
     cin >> innerSize;
 
-    if (innerSize >= outerSize) {
-        cerr << "Rozmiar wewnetrzny musi byc mniejszy niz zewnetrzny!\n";
+    if (innerSize >= outerSize || ((outerSize % 2 == 0 && innerSize % 2 != 0) || (outerSize % 2 != 0 && innerSize % 2 == 0))) {
+        cerr << "Rozmiar wewnetrzny musi byc mniejszy niz zewnetrzny oraz posiadać identyczną parzystość (symetria)!\n";
         return 1;
     }
 
