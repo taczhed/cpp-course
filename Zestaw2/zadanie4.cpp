b622687b963a935329b4a953bf2f5507f20280c6
Patryk Lach <patryk.lach@mp.pl>
2023-03-28 09:48:16 +0200

    fix(zadanie4): function remove

--- Zestaw2/zadanie4.cpp
+++ Zestaw2/zadanie4.cpp
@@ -5,22 +5,6 @@ using namespace std;
 
 // /home/taczhed/Systemy/Zadanie1/src
 
-void countFiles(string dirPath) {
-
-    bool isEmpty = true;
-
-    for (auto file : filesystem::directory_iterator(dirPath)) {
-        if (filesystem::is_regular_file(file)) {
-            cout << file.path().filename().string() << "\t" << filesystem::file_size(file) << " bajtow" << endl;
-            isEmpty = false;
-        }
-    }   
-
-    if (isEmpty) {
-        cout << "W podanym katalogu nie ma zwyklych plikow" << endl;
-    }
-}
-
 int main() {
     string path;
     bool isEmpty = true;
